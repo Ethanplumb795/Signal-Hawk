868f9e252349427a7a1854c0ed2e7958fe0ba250
Ethan Plumb <ethanplumb795@gmail.com>
2023-02-28 18:06:11 -0700

    added more to the hello world

--- mavsdk_test/hello_mavsdk.py
+++ mavsdk_test/hello_mavsdk.py
@@ -1,4 +1,11 @@
 from mavsdk import System
 
+# 
 drone = System()
+
+# Connect
 await drone.connect(system_address="udp://:14540")
+
+# Arm and takeoff
+await drone.action.arm()
+await drone.action.takeoff()
