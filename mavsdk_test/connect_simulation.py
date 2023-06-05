86df93163d447945ba18f94101ba17ba19e5959d
Ethan Plumb <ethanplumb795@gmail.com>
2023-06-05 14:13:13 -0700

    Ability to send and recv messages added

--- mavsdk_test/connect_simulation.py
+++ mavsdk_test/connect_simulation.py
@@ -9,3 +9,15 @@ the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 # Once connected, use 'the_connection' to get and send messages
+try:
+    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+    timestamp = the_connection.time_since('GPS_RAW_INT')
+    print(f"Timestamp: {timestamp}\nAltitude: {altitude}")
+except:
+    print('No GPS_RAW_INT message received')
+
+try:
+    msg = the_connection.recv_match(type='SYS_STATUS',blocking=True)
+    print(f"Status: {msg}\n")
+except:
+    print("No SYS_STATUS received.\n")
